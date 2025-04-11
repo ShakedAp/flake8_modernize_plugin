a05aec5a7e56591c02a8e99b0cf39bb0c3febb0d
ShakedAp <shaked.apter123@gmail.com>
2025-04-11 23:49:48 +0300

    Add flake8 configuration - not working

--- flake8_modernize_plugin/flake8_plugin.py
+++ flake8_modernize_plugin/flake8_plugin.py
@@ -4,27 +4,33 @@ from cloned_modernize.modernize.fixes import fissix_fix_names, opt_in_fix_names,
 import logging
 
 
-if __name__ == '__main__':
+class ModernizeCompatibilityPlugin:
+    name = "modernize_compatibility_plugin"
+    version = "1.0.0"
 
-    logging.basicConfig(format="%(name)s: %(message)s", level=logging.ERROR)
+    def __init__(self, tree, lines):
+        self._tree = tree
+        self._lines = lines
 
-    options = {
-        "print_function": False,
-        "exec_function": True,
-        "write_unchanged_files": False
-    }
+    def run(self):
+        logging.basicConfig(format="%(name)s: %(message)s", level=logging.ERROR)
 
-    fixer_pkg = "cloned_modernize.modernize.fixes"
-    avail_fixes = set(get_fixers_from_package(fixer_pkg))
-    avail_fixes.update(fissix_fix_names)
-    avail_fixes.update(opt_in_fix_names)
-    avail_fixes.update(six_fix_names)
-    # avail_fixes.add("cloned_modernize.modernize.fixes.fix_unicode")
-    # avail_fixes.add("cloned_modernize.modernize.fixes.fix_unicode_future")
+        options = {
+            "print_function": False,
+            "exec_function": True,
+            "write_unchanged_files": False
+        }
 
-    rt = RefactoringTool(sorted(avail_fixes), options=options)
+        fixer_pkg = "cloned_modernize.modernize.fixes"
+        avail_fixes = set(get_fixers_from_package(fixer_pkg))
+        avail_fixes.update(fissix_fix_names)
+        avail_fixes.update(opt_in_fix_names)
+        avail_fixes.update(six_fix_names)
+        # avail_fixes.add("cloned_modernize.modernize.fixes.fix_unicode")
+        # avail_fixes.add("cloned_modernize.modernize.fixes.fix_unicode_future")
 
-    rt.refactor(['.'])
-    print(rt.flake8_errors)
+        rt = RefactoringTool(sorted(avail_fixes), options=options)
 
-    return_code = int(bool(rt.errors))
+        rt.refactor_string(''.join(self._lines))
+
+        yield from rt.flake8_errors
